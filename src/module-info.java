f257d11a4a806760114b153be4316243f7385aca
fr3d3lin <114347855+fr3d3lin@users.noreply.github.com>
2024-11-20 22:14:11 -0500

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GrisesiaRpg_Combat {
+	requires spigot;
+	requires Multiverse.Portals;
+	requires Multiverse.Core;
+	requires WorldGuardEvents;
+	requires worldguard.bukkit;
+	requires worldedit.bukkit;
+}
